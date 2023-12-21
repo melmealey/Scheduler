4bfd00841a17df30ec101cab88d86bd3b7fe43ce
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-20 22:29:32 -0500

    added base for README file and .js

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Scheduler
+## Scheduler
+
+<img width="436" alt="Screenshot 2023-12-18 at 3 56 45 PM" src="https://github.com/melmealey/Quiz/assets/147653410/dcb119e2-011e-4d13-a823-7872efd7d2e9">
+
+## Description
+
+
+
+## Installation
+
+N/A
+
+## Usage
+
+
+https://melmealey.github.io/Scheduler/
+
+
+## Credits
+
+
+
+
+## License
+
+Please refer to the LICENSE in the repo.
---
+++ script.js
@@ -0,0 +1,33 @@
+// display todays date
+
+//dayjs prob use now and format (read the docs)
+//get todays date in the right format and save to a variable
+//console.log that variable
+//append to page
+        //create an element
+        //set inner text to that vairable i just made
+        //append that to the currentDay div
+
+//use dayjs to get the curret hour and store it in a variable
+// day.js() hour()
+
+const divs =document.querySelectorAll(`.py-3`) //this is an array
+console.log(divs)
+// for loop
+    //get inner test from each div
+    //compare that to the current hour (if statement)
+        //if inner text is less than current hour
+        //.setAttribute('class', 'row time block past')
+        //if inner text is equal to than current hour
+        //.setAttribute('class', row time-block present')
+        //...
+
+    //save things in localstorage
+    //get value from text input
+    //save to local storage ( also save the time! as an object)
+
+
+
+
+
+
