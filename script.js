d396cf030e8c443cab1d4f4c1bc6feff24adf959
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-21 22:11:56 -0500

    working on getting the current date on the page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // display todays date
+var currentDay =dayjs()
 
 //dayjs prob use now and format (read the docs)
 //get todays date in the right format and save to a variable
@@ -11,8 +12,8 @@
 //use dayjs to get the curret hour and store it in a variable
 // day.js() hour()
 
-const divs =document.querySelectorAll(`.py-3`) //this is an array
-console.log(divs)
+// const divs =document.querySelectorAll(`.py-3`) //this is an array
+// console.log(divs)
 // for loop
     //get inner test from each div
     //compare that to the current hour (if statement)
