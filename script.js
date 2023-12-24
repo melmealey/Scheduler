9de8a60cfaa3fa5aca100ea8cd7c00ba62b0660f
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-23 22:09:45 -0500

    got current date appended

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
 // display todays date
-let currentDay = dayjs();
+let date = dayjs().format('MMMM-DD-YYYY');
+console.log(date);
 
-console.log(currentDay);
+let currentDay = document.getElementById('currentDay');
 
-const currentDay = document.getElementById("currentDay");
-const paragraph = document.createElement("p");
-paragraph.textContent = "This is some text from the console.";
+let paragraph = document.createElement("p");
+paragraph.innerText = date;
 currentDay.appendChild(paragraph);
 
 
-const myDiv = document.getElementById("myDiv");
-
-// Step 2: Create a new element
-const paragraph = document.createElement("p");
-paragraph.textContent = "This is some text from the console.";
-
-// Step 3: Append the element to the selected element
-myDiv.appendChild(paragraph);
-
-
-//dayjs prob use now and format (read the docs)
-//get todays date in the right format and save to a variable
-//console.log that variable
-//append to page
-        //create an element
-        //set inner text to that vairable i just made
-        //append that to the currentDay div
-
-//use dayjs to get the curret hour and store it in a variable
-// day.js() hour()
+//use dayjs to get the current hour and store it in a variable
+let currentHour = dayjs().format('h A');
+console.log(currentHour);
 
 // const divs =document.querySelectorAll(`.py-3`) //this is an array
 // console.log(divs)
 // for loop
-    //get inner test from each div
-    //compare that to the current hour (if statement)
-        //if inner text is less than current hour
-        //.setAttribute('class', 'row time block past')
-        //if inner text is equal to than current hour
-        //.setAttribute('class', row time-block present')
-        //...
+//     get inner test from each div
+//     compare that to the current hour (if statement)
+//         if inner text is less than current hour
+//         .setAttribute('class', 'row time block past')
+//         if inner text is equal to than current hour
+//         .setAttribute('class', row time-block present')
+//         if inner text is more than the current hour
+//         .setAttribute ('class', row time block future)
 
-    //save things in localstorage
-    //get value from text input
-    //save to local storage ( also save the time! as an object)
+
+//                     // To check the time and add the classes for background indicators
+//                     if (blockTime < timeNow) {
+//                         $(this).removeClass("future");
+//                         $(this).removeClass("present");
+//                         $(this).addClass("past");
+//                     }
+//                     else if (blockTime === timeNow) {
+//                         $(this).removeClass("past");
+//                         $(this).removeClass("future");
+//                         $(this).addClass("present");
+//                     }
+//                     else {
+//                         $(this).removeClass("present");
+//                         $(this).removeClass("past");
+//                         $(this).addClass("future");
+
+//                     }
+
+//     save things in localstorage
+//     get value from text input
+//     save to local storage ( also save the time! as an object)
 
 
 
