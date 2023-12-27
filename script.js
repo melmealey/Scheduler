d4a09954e82882faba102a28fc8a597b387facfd
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-26 23:06:19 -0500

    trying to lear JSON and local storage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // display todays date
 let date = dayjs().format('MMMM-DD-YYYY');
-console.log(date);
+// console.log(date);
 
 let currentDay = document.getElementById('currentDay');
 
@@ -9,22 +9,22 @@ paragraph.innerText = date;
 currentDay.appendChild(paragraph);
 
 let currentHour = dayjs().format('hA');
-console.log(currentHour);
+// console.log(currentHour);
 
 
 const hours = ['1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM', '12PM'];
 
-console.log (hours);
+// console.log (hours);
 
 function checkHour() {
   for (let i = 0; i < hours.length; i++) {
  
     let element = document.querySelectorAll('.py-3')[i];
     let parent = element.closest('.row');
-    console.log(parent);
-    console.log(element.innerText);
-    console.log(currentHour);
-    console.log(element.innerText === currentHour);
+    // console.log(parent);
+    // console.log(element.innerText);
+    // console.log(currentHour);
+    // console.log(element.innerText === currentHour);
 
     if (element.innerText === currentHour) {
       parent.setAttribute('class', 'row time-block present');
@@ -38,49 +38,28 @@ function checkHour() {
 
 checkHour ();
 
-// function makePlans() {
+//Enter data in hour blocks that will be saved to local storage
+function makePlans() {
 
-//     $(".hour").each(function() {
-//         var currHour = $(this).text();
-//         var currPlan = localStorage.getItem(currHour);
 
-//         // console.log(this);
 
-//         if(currPlan !== null) {
-//             $(this).siblings(".plan").val(currPlan);
-//         }
-//     });
-// }
+}
 
- //grabs values from time and value divs and saves them to local storage
-//  $(".saveBtn").click(function (event) {
-//     event.preventDefault();
-//     var value = $(this).siblings(".time-block").val();
-//     var time = $(this).parent().attr("id").split("-")[1];
-//     localStorage.setItem(time, value);
-//   });
+// const userInput = document.querySelector ('textarea');
+// localStorage.setItem ('input', JSON.stringify (userInput));
 
-//   //retrieves items from local storage and sets them in proper places
-//   $("#hour-09 .time-block").val(localStorage.getItem("09"));
-//   $("#hour-10 .time-block").val(localStorage.getItem("10"));
-//   $("#hour-11 .time-block").val(localStorage.getItem("11"));
-//   $("#hour-12 .time-block").val(localStorage.getItem("12"));
-//   $("#hour-13 .time-block").val(localStorage.getItem("13"));
-//   $("#hour-14 .time-block").val(localStorage.getItem("14"));
-//   $("#hour-15 .time-block").val(localStorage.getItem("15"));
-//   $("#hour-16 .time-block").val(localStorage.getItem("16"));
-//   $("#hour-17 .time-block").val(localStorage.getItem("17"));
-// });
+// let inputs = localStorage.getItem ('inputs');
+// localStorage.getItem ('inputs', JSON.parse (userInput));
 
-// makePlans();
 
-// const data = {};
-// const dataString = JSON.stringify(data);
-// localStorage.setItem("userData", dataString);
+const userInput = document.querySelector('#textarea').value; 
+const inputObject = { input: userInput };
+localStorage.setItem('input', JSON.stringify(inputObject));
 
-// const storedDataString = localStorage.getItem("userData");
-// const storedData = JSON.parse(storedDataString);
-// console.log(storedData);
+const storedInput = localStorage.getItem('input');
+const parsedInput = JSON.parse(storedInput);
+
+saveBtn.addEventListenr ('click',save)
 
 
 //     save things in localstorage
